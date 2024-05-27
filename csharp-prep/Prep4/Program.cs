91b3f803b68dccbed6a2b91835db3e8b2c315129
sophia62 <sophiabeebe1@gmail.com>
2024-05-27 17:03:49 -0600

    Finished prep late

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int numberEntered = -1;
+
+        while (numberEntered != 0) 
+{
+       Console.Write("Enter some numbers, then put 0 when you want to stop.");
+
+         string input= Console.ReadLine();
+         numberEntered = int.Parse(input);
+        if (numberEntered != 0)
+    {
+        numbers.Add(numberEntered);
     }
+
+}
+//Sum the numbers
+ {
+int sum = 0;
+
+for (int i = 0; i <numbers.Count; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine ("Sum:"+ sum);
+
+//average of numbers in list used from example code after trying  same with max 
+float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+    }
+}
 }
