5cdb2b10969c7cce73677dab69e930b810b22dc4
sophia62 <sophiabeebe1@gmail.com>
2024-05-27 16:11:45 -0600

    THis is my lesson 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,33 +8,33 @@ class Program
         Console.WriteLine("What is your percentage score?");
         string percentageString = Console.ReadLine();
         int percentage = int.Parse(percentageString);
-    //  if percentage = <93
-    if (percentage >=93)
-    {
-        Console.WriteLine("A");
-    }
-    //  if percentage is = 80-92
-     if (percentage <93 && percentage >=80)
-    {
-        Console.WriteLine("B");
-    }
-    //  if percentage is 79-60
-     if (percentage <79 && percentage >=70)
-    {
-        Console.WriteLine("C");
-    }
-      if (percentage <69 && percentage >=61)
-    {
-        Console.WriteLine("D");
-    }
-    //  else if percentage is 60
-    if (percentage <=60)
-    {
-        Console.WriteLine("F");
-    }
+        //  if percentage = <93
+        if (percentage >= 93)
+        {
+            Console.WriteLine("A");
+        }
+        //  if percentage is = 80-92
+        if (percentage < 93 && percentage >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        //  if percentage is 79-60
+        if (percentage < 79 && percentage >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        if (percentage < 69 && percentage >= 61)
+        {
+            Console.WriteLine("D");
+        }
+        //  else if percentage is 60
+        if (percentage <= 60)
+        {
+            Console.WriteLine("F");
+        }
 
 
 
-        
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,80 @@
+//All of the below is wrong code that I tried but below is the correct code. 
+
+// using System;
+// using System.Globalization;
+// {
+// Console.WriteLine("Hello Prep3 World!");
+// Console.WriteLine("What is the Magic Number?");
+// Random random = new Random();
+// int number = random.Next(1, 100);
+// int min = 0;
+// int max = 100;
+// int guess = 0;
+// int realnumber;
+
+
+// {
+//         realnumber = random.Next(min, max);
+
+//     Console.WriteLine("Guess any number between 0 and 100");
+
+//     while (guess != realnumber )
+//     guess = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Guess: " + guess);
+
+//     if (guess > realnumber)
+//     {
+//         Console.WriteLine("Guess is too low");
+//     }
+//     else if (guess < realnumber)
+//     {
+//     Console.WriteLine("guess is too high");
+//     }
+//     else 
+//     {
+//     Console.WriteLine("Thats Correct!");
+//     }
+
+//     Console.WriteLine ("Number" + realnumber);
+//     Console.WriteLine("You Win");
+
+
+
+
+// }
+// }
+
 using System;
 
-class Program
+class GuessingGame
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        
+        int guess = 0;
+
+        // Loop
+        for (; guess != magicNumber;)
+        {
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
